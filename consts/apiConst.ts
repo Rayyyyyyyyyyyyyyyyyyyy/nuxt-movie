290b0a2cd447ed3dd0fc7257ee09d36d0ca2e96a
gordon.shao <gordon.shao@dkdtrillion.com>
2024-10-04 17:06:56 +0800

    fix api url

--- consts/apiConst.ts
+++ consts/apiConst.ts
@@ -1,6 +1,6 @@
 export enum EApiPaths {
-  popularList = "/movie/popular",
-  topRateList = "/movie/top_rated",
+  popularList = "movie/popular",
+  topRateList = "movie/top_rated",
   upComingList = "movie/upcoming",
   nowPlaying = "movie/now_playing",
   movieGener = "discover/movie",
@@ -8,7 +8,7 @@ export enum EApiPaths {
   personDetail = "person/",
 
   tvPopularList = "tv/popular",
-  tvTopRateList = "/tv/top_rated",
+  tvTopRateList = "tv/top_rated",
   tvAriring = "tv/airing_today",
   tvOnAir = "tv/on_the_air",
 
